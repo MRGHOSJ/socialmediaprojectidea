d8e8d82b6b799fa7539fddcdfa85fdc9c46eae91
MRGHOSJ <bouzouitayassine@gmail.com>
2019-05-29 13:49:35 +0100

    Updates

---
+++ precache-manifest.e2a7baf3c9e4e71021c07b1b15e0149c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370aa4dc37bd579a8b9a0423d07d8c67",
+    "url": "/myappcopyfb/index.html"
+  },
+  {
+    "revision": "055b2a121e96178ac1c3",
+    "url": "/myappcopyfb/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "33dc9e755836d5e96f05",
+    "url": "/myappcopyfb/static/css/main.80ece895.chunk.css"
+  },
+  {
+    "revision": "055b2a121e96178ac1c3",
+    "url": "/myappcopyfb/static/js/2.8603bda2.chunk.js"
+  },
+  {
+    "revision": "33dc9e755836d5e96f05",
+    "url": "/myappcopyfb/static/js/main.7e483b0c.chunk.js"
+  },
+  {
+    "revision": "0305e23adbabd2e3cf92",
+    "url": "/myappcopyfb/static/js/runtime~main.bf59ce3b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app-copyfb/precache-manifest.c119863671d88df94215e6db43281219.js"
+  "/myappcopyfb/precache-manifest.e2a7baf3c9e4e71021c07b1b15e0149c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app-copyfb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myappcopyfb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
