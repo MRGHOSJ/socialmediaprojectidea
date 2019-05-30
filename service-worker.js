9167cb19331899ce21520128a339cc3cef14a39e
MRGHOSJ <bouzouitayassine@gmail.com>
2019-05-30 17:09:06 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/socialmediaprojectidea/precache-manifest.73cdf2534b1e801fc8c3243b4f6d14f9.js"
+  "/socialmediaprojectidea/precache-manifest.856df30bcdc4237c2029c6b54953f9e0.js"
 );
 
 self.addEventListener('message', (event) => {
